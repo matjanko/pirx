cc67b8cc67e89a2988ca66a1d96af3a6daecb422
matjanko <mateusz.jankowski@prokonto.pl>
2022-10-05 18:40:12 +0200

    PIRX-1

---
+++ src/Modules/Expenses/Pirx.Modules.Expenses.Core/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: InternalsVisibleTo("Pirx.Modules.Expenses.Api")]
+namespace Pirx.Modules.Expenses.Core;
+
+internal static class Extensions
+{
+    public static IServiceCollection AddCore(this IServiceCollection services)
+    {
+        return services;
+    }
+}
---
+++ src/Shared/Pirx.Shared.Infrastructure/Modules/ModuleLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using Pirx.Shared.Abstractions.Modules;
+
+[assembly: InternalsVisibleTo("Pirx.Launcher")]
+namespace Pirx.Shared.Infrastructure.Modules;
+
+internal static class ModuleLoader
+{
+    public static IEnumerable<Assembly> LoadModuleAssemblies()
+    {
+        var assemblies = AppDomain.CurrentDomain.GetAssemblies().ToList();
+        var locations = assemblies.Where(x => !x.IsDynamic).Select(x => x.Location).ToArray();
+        var files = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory, "*.dll")
+            .Where(x => !locations.Contains(x, StringComparer.InvariantCultureIgnoreCase) && x.Contains("Pirx.Modules.")).ToList();
+
+        return files.Select(x => AppDomain.CurrentDomain.Load(AssemblyName.GetAssemblyName(x))).ToList();
+    }
+
+    public static IEnumerable<IModule> LoadModules(IEnumerable<Assembly> assemblies)
+    {
+        var modules = assemblies
+            .SelectMany(x => x.GetTypes())
+            .Where(x => typeof(IModule).IsAssignableFrom(x) && !x.IsInterface)
+            .OrderBy(x => x.Name)
+            .Select(Activator.CreateInstance)
+            .Cast<IModule>()
+            .ToList();
+
+        return modules;
+    }
+}
